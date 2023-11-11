e904233048b93f51a991c310f6430d406058ba08
Yong-yeon Kim <70844774+lotus-yeon@users.noreply.github.com>
2023-11-12 01:07:32 +0900

    fix: separate numeric validation from string validation

--- src/InputView.js
+++ src/InputView.js
@@ -1,3 +1,4 @@
+import { Console } from "@woowacourse/mission-utils";
 import { ERROR } from "./constants/error.js";
 import { INPUT } from "./constants/input.js";
 import { throwErrorIf } from "./utils/index.js";
@@ -10,16 +11,18 @@ const TYPE = {
 const InputView = {
   async readDate() {
     const input = await Console.readLineAsync(INPUT.DATE);
+    this.validate(TYPE.DATE, input);
 
     return parseInt(input);
   },
 
   validate(type, value) {
     if (type === TYPE.DATE) {
+      const date = Number(value);
       const isNotValidDate =
-        isNaN(value) || !Number.isInteger(value) || (value >= 1 && value <= 31);
+        isNaN(date) || !Number.isInteger(date) || date < 1 || date > 31;
 
-      throwErrorIf(value.length === 0, ERROR.NO_INPUT);
+      throwErrorIf(value.length === 0, ERROR.NO_DATE);
       throwErrorIf(isNotValidDate, ERROR.INVALID_DATE);
     }
   },
