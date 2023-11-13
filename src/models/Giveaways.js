7bd651c29ce3b48659b0237543573dfd0d8722fb
Yong-yeon Kim <70844774+lotus-yeon@users.noreply.github.com>
2023-11-13 21:23:14 +0900

    feat: implement Giveways object

---
+++ src/models/Giveaways.js
@@ -0,0 +1,24 @@
+import { MENU } from "../constants/menu.js";
+import { OUTPUT } from "../constants/output.js";
+
+const Giveaways = {
+  gift: {
+    name: MENU.CHAMPAGNE,
+    count: 1,
+    price: 25000,
+  },
+  requiredPrice: 120000,
+
+  // 총주문 금액에 따라 증정품을 준다.
+  giveBasedOn(price) {
+    let gift = { name: OUTPUT.NOTHING, price: 0, count: 0 };
+
+    if (price >= this.requiredPrice) {
+      gift = Object.assign(this.gift);
+    }
+
+    return gift;
+  },
+};
+
+export default Giveaways;
