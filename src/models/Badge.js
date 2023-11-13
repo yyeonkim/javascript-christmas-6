f8b43492c370d8677e17c5089c1273984425a20e
Yong-yeon Kim <70844774+lotus-yeon@users.noreply.github.com>
2023-11-14 00:44:44 +0900

    fix: sort requiredDiscount in descending order

--- src/models/Badge.js
+++ src/models/Badge.js
@@ -2,9 +2,9 @@ import { BADGE } from "../constants/badge.js";
 
 const Badge = {
   requiredDiscount: {
-    [BADGE.STAR]: 5000,
-    [BADGE.TREE]: 10000,
     [BADGE.SANTA]: 20000,
+    [BADGE.TREE]: 10000,
+    [BADGE.STAR]: 5000,
   },
 
   // 총혜택 금액에 따라 배지룰 부여한다.
