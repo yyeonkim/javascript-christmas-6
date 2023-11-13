2df6064a9f53c5c354b50dc8b85b02cb3bfc9b01
Yong-yeon Kim <70844774+lotus-yeon@users.noreply.github.com>
2023-11-13 18:43:12 +0900

    test: add test for Menu class

---
+++ __tests__/MenuTest.js
@@ -0,0 +1,18 @@
+import { MENU, MENU_TYPE } from "../src/constants/menu.js";
+import Menu from "../src/models/Menu.js";
+
+describe("메뉴 클래스 테스트", () => {
+  let soup;
+  beforeAll(() => {
+    soup = new Menu(MENU.SOUP);
+  });
+
+  test("메뉴 개수 만큼 주문 금액을 계산한다.", () => {
+    const price = soup.computePrice(2);
+    expect(price).toEqual(12000);
+  });
+
+  test("메뉴의 유형을 알려준다.", () => {
+    expect(soup.getType()).toEqual(MENU_TYPE.APPETIZER);
+  });
+});
