10480fa2b2ac8d83a9aa18ef74452b88e8632b05
Yong-yeon Kim <70844774+lotus-yeon@users.noreply.github.com>
2023-11-13 22:26:08 +0900

    test: add test for weekday discount

---
+++ __tests__/WeekdayDiscountTest.js
@@ -0,0 +1,30 @@
+import { DAY } from "../src/constants/day.js";
+import WeekdayDiscount from "../src/models/WeekdayDiscount.js";
+
+describe("평일 할인 테스트", () => {
+  const applied = [
+    [2, DAY.SUNDAY, 4046],
+    [1, DAY.MONDAY, 2023],
+    [0, DAY.TUESDAY, 0],
+    [3, DAY.THURSDAY, 6069],
+  ];
+
+  test.each(applied)(
+    "일요일부터 목요일까지 평일 할인이 적용된다.",
+    (dessertCount, day, discount) => {
+      expect(WeekdayDiscount.giveBasedOn(dessertCount, day)).toEqual(discount);
+    }
+  );
+
+  const notApplied = [
+    [2, DAY.FRIDAY, 0],
+    [3, DAY.SATURDAY, 0],
+  ];
+
+  test.each(notApplied)(
+    "금요일과 토요일에는 평일 할인이 적용되지 않는다.",
+    (dessertCount, day, discount) => {
+      expect(WeekdayDiscount.giveBasedOn(dessertCount, day)).toEqual(discount);
+    }
+  );
+});
