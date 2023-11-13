d95229253447cfad5b7c45a471bd130231c2afc4
Yong-yeon Kim <70844774+lotus-yeon@users.noreply.github.com>
2023-11-13 22:26:24 +0900

    test: add more cases to weekend discount

--- __tests__/WeekendDiscountTest.js
+++ __tests__/WeekendDiscountTest.js
@@ -2,19 +2,30 @@ import { DAY } from "../src/constants/day.js";
 import WeekendDiscount from "../src/models/WeekendDiscount.js";
 
 describe("주말 할인 테스트", () => {
-  const cases = [
+  const applied = [
     [2, DAY.FRIDAY, 4046],
     [1, DAY.SATURDAY, 2023],
   ];
 
-  test.each(cases)(
+  test.each(applied)(
     "금요일과 토요일에는 주말 할인이 적용된다.",
     (mainCount, day, discount) => {
       expect(WeekendDiscount.giveBasedOn(mainCount, day)).toEqual(discount);
     }
   );
 
-  test("금요일이나 토요일이 아니면 주말 할인이 적용되지 않는다.", () => {
-    expect(WeekendDiscount.giveBasedOn(3, DAY.MONDAY)).toEqual(0);
-  });
+  const notApplied = [
+    [2, DAY.SUNDAY, 0],
+    [1, DAY.MONDAY, 0],
+    [1, DAY.TUESDAY, 0],
+    [3, DAY.WEDNESDAY, 0],
+    [6, DAY.THURSDAY, 0],
+  ];
+
+  test.each(notApplied)(
+    "금요일이나 토요일이 아니면 주말 할인이 적용되지 않는다.",
+    (mainCount, day, discount) => {
+      expect(WeekendDiscount.giveBasedOn(3, DAY.MONDAY)).toEqual(0);
+    }
+  );
 });
