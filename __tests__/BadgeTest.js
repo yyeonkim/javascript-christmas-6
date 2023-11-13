dbaa881ae8cd57ff38ae25f132170d67eb78654b
Yong-yeon Kim <70844774+lotus-yeon@users.noreply.github.com>
2023-11-14 00:44:59 +0900

    test: add test for granting Badge

---
+++ __tests__/BadgeTest.js
@@ -0,0 +1,14 @@
+import { BADGE } from "../src/constants/badge.js";
+import Badge from "../src/models/Badge.js";
+
+describe("배지 테스트", () => {
+  const cases = [
+    [30000, BADGE.SANTA],
+    [15000, BADGE.TREE],
+    [6000, BADGE.STAR],
+  ];
+
+  test.each(cases)("총혜택 금액에 따라 배지를 부여한다.", (discount, badge) => {
+    expect(Badge.grant(discount)).toEqual(badge);
+  });
+});
