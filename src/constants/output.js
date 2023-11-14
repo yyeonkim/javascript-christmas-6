941a8aa5c13d1ad41c0b678740f3a373cc4eca58
yyeonkim <seyeon.dev@gmail.com>
2023-11-14 14:00:16 +0900

    feat: implement OutputView

--- src/constants/output.js
+++ src/constants/output.js
@@ -8,4 +8,9 @@ export const OUTPUT = {
   FINAL_PRICE: "<할인 후 예상 결제 금액>",
   BADGE: "<12월 이벤트 배지>",
   NOTHING: "없음",
+  D_DAY_DISCOUNT: "크리스마스 디데이 할인",
+  WEEKDAY_DISCOUNT: "평일 할인",
+  SPECIAL_DISCOUNT: "특별 할인",
+  GIVEAWAYS_EVENT: "증정 이벤트",
+  WEEKEND_DISCOUNT: "주말 할인",
 };
--- src/views/OutputView.js
+++ src/views/OutputView.js
@@ -1,9 +1,92 @@
+import { Console } from "console";
+import { EOL as LINE_SEPARATOR } from "os";
+import { OUTPUT } from "../constants/output";
+
 const OutputView = {
-  printMenu() {
-    Console.print("<주문 메뉴>");
-    // ...
+  printPreview() {
+    Console.print(OUTPUT.TITLE + LINE_SEPARATOR);
   },
-  // ...
+
+  /**
+   * 주문 메뉴 출력
+   * @param {Object[]} order
+   * @param {string} order[].name - 메뉴 이름
+   * @param {number} order[].count - 메뉴 개수
+   */
+  printOrder(order) {
+    Console.print(OUTPUT.ORDER);
+
+    let str = "";
+    order.forEach((item) => {
+      str += `${item.name} ${item.count}개` + LINE_SEPARATOR;
+    });
+
+    Console.print(str);
+  },
+
+  printPriceBeforeDiscount(price) {
+    Console.print(OUTPUT.PRICE_BEFORE_DISCOUNT);
+    Console.print(`${price}원` + LINE_SEPARATOR);
+  },
+
+  /**
+   * 증정 메뉴 출력
+   * @param {Object} giveaways
+   * @param {string} giveaways.name
+   * @param {number} giveaways.count
+   */
+  printGiveaways(giveaways) {
+    Console.print(OUTPUT.GIVEAWAYS);
+
+    let str = OUTPUT.NOTHING;
+    if (giveaways.name !== OUTPUT.NOTHING) {
+      str = `${giveaways.name} ${giveaways.count}개`;
+    }
+    str += LINE_SEPARATOR;
+
+    Console.print(str);
+  },
+
+  /**
+   * 혜택 내역 출력
+   * @param {Object} event
+   * @param {number} event[OUTPUT.D_DAY_DISCOUNT]
+   * @param {number} event[OUTPUT.WEEKDAY_DISCOUNT]
+   * @param {number} event[OUTPUT.WEEKEND_DISCOUNT]
+   * @param {number} event[OUTPUT.GIVEAWAYS_EVENT]
+   * @param {number} event[OUTPUT.SPECIAL_DISCOUNT]
+   */
+  printDiscountPerEvent(event) {
+    Console.print(OUTPUT.DISCOUNT);
+
+    let str = "";
+    for (const [key, value] of Object.entries(event)) {
+      if (value !== 0) str += `${key}: -${value}원` + LINE_SEPARATOR;
+    }
+
+    if (str.length === 0) str = OUTPUT.NOTHING;
+    Console.print(str);
+  },
+
+  printTotalDiscount(price) {
+    Console.print(OUTPUT.TOTLA_DISCOUNT);
+
+    let str = "0원";
+    if (price !== 0) str = `-${price}원`;
+
+    Console.print(str + LINE_SEPARATOR);
+  },
+
+  /* 할인 후 예상 결제 금액 출력 */
+  printFinalPrice(price) {
+    Console.print(OUTPUT.FINAL_PRICE);
+    Console.print(`${price}원` + LINE_SEPARATOR);
+  },
+
+  printBadge(badge) {
+    Console.print(OUTPUT.BADGE);
+    Console.print(badge + LINE_SEPARATOR);
+  },
 };
 
 export default OutputView;
