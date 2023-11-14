dfb064b3cfaf115f1608d5a0c060aa09bc5abcab
Yong-yeon Kim <70844774+lotus-yeon@users.noreply.github.com>
2023-11-14 22:53:12 +0900

    chore: remove double quotes

--- src/constants/error.js
+++ src/constants/error.js
@@ -1,9 +1,9 @@
 const PREFIX = "[ERROR]";
 
 export const ERROR = {
-  NO_DATE: `${PREFIX} 날짜가 입력되지 않았습니다. "다시 입력해 주세요."`,
-  INVALID_DATE: `${PREFIX} 유효하지 않은 날짜입니다. "다시 입력해 주세요."`,
-  INVALID_ORDER: `${PREFIX} 유효하지 않은 주문입니다. "다시 입력해 주세요."`,
+  NO_DATE: `${PREFIX} 날짜가 입력되지 않았습니다. 다시 입력해 주세요.`,
+  INVALID_DATE: `${PREFIX} 유효하지 않은 날짜입니다. 다시 입력해 주세요.`,
+  INVALID_ORDER: `${PREFIX} 유효하지 않은 주문입니다. 다시 입력해 주세요.`,
   NOT_ONLY_BEVERAGES: `${PREFIX} 음료만 주문할 수 없습니다. 다시 주문해 주세요.`,
   TOO_MANY_MENU_ORDERED: `${PREFIX} 메뉴는 최대 20개까지만 주문할 수 있습니다. 다시 주문해 주세요.`,
 };
