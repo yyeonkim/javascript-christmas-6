e0b683bcce2893641d5968a857c2363edfe70524
Yong-yeon Kim <70844774+lotus-yeon@users.noreply.github.com>
2023-11-15 17:32:47 +0900

    feat: apply event when orderPrice is fulfilled

--- src/controllers/EventController.js
+++ src/controllers/EventController.js
@@ -9,6 +9,7 @@ import { EVENT } from "../constants/event.js";
 class EventController {
   #orderPrice;
   #requiredPrice = 10000;
+  #isAvaliable = false;
   #date;
   // 유형별 주문한 메뉴 개수
   #menuCountPerType = {
@@ -21,18 +22,18 @@ class EventController {
   constructor(date, orderPrice, menuCountPerType) {
     this.#date = date;
     this.#orderPrice = orderPrice;
+    this.#isAvaliable = orderPrice >= this.#requiredPrice;
     this.#menuCountPerType = menuCountPerType;
   }
 
   // 총혜택 금액을 계산한다.
   computeTotal() {
-    if (this.#orderPrice >= this.#requiredPrice) {
+    if (this.#isAvaliable) {
       const discount = this.computeEach();
       const total = Object.values(discount).reduce(
         (acc, current) => acc + current,
         0
       );
-
       return total;
     }
     return 0;
@@ -40,13 +41,16 @@ class EventController {
 
   // 이벤트별 혜택 내역을 계산한다.
   computeEach() {
-    return {
-      [EVENT.D_DAY_DISCOUNT]: this.#computeDDay(),
-      [EVENT.WEEKDAY_DISCOUNT]: this.#computeWeekday(),
-      [EVENT.WEEKEND_DISCOUNT]: this.#computeWeekend(),
-      [EVENT.GIVEAWAYS_EVENT]: this.#computeGiveaways(),
-      [EVENT.SPECIAL_DISCOUNT]: this.#computeSpecial(),
-    };
+    if (this.#isAvaliable) {
+      return {
+        [EVENT.D_DAY_DISCOUNT]: this.#computeDDay(),
+        [EVENT.WEEKDAY_DISCOUNT]: this.#computeWeekday(),
+        [EVENT.WEEKEND_DISCOUNT]: this.#computeWeekend(),
+        [EVENT.GIVEAWAYS_EVENT]: this.#computeGiveaways(),
+        [EVENT.SPECIAL_DISCOUNT]: this.#computeSpecial(),
+      };
+    }
+    return this.#initEvents();
   }
 
   #computeDDay() {
@@ -64,6 +68,18 @@ class EventController {
   }
   #computeSpecial() {
     return SpecialDiscount.giveIf(this.#date);
+  }
+
+  // 이벤트별 혜택 내역이 모두 0인 obj 반환
+  #initEvents() {
+    const eventTypes = Object.values(EVENT);
+    let events = {};
+
+    eventTypes.forEach((type) => {
+      events[type] = 0;
+    });
+
+    return events;
   }
 }
 
